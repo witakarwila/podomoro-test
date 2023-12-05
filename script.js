97b8603b613d50a50fbdb2f766d730ef64208bc2
witakarwila <wita@teknologicakrainternasional.com>
2023-12-05 13:01:26 +0700

    button ar

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ const onProgress = (event) => {
     progressBar.classList.remove('hide');
   }
 };
+<<<<<<< HEAD
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+=======
+
+const handleARButtonClick = () => {
+  if (document.querySelector('model-viewer').canActivateAR) {
+    document.querySelector('model-viewer').activateAR();
+  } else {
+    console.error('AR mode is not supported.');
+    alert('AR mode is not supported on your device');
+  }
+};
+
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+document.querySelector('#ar-button').addEventListener('click', handleARButtonClick);
+>>>>>>> 3cc2cc1 (button ar)
